7fa692dd4eeef7be8a9d9a07bdabdd91ba346e3c
yusufr98 <yusuf090798@gmail.com>
2019-12-06 10:36:53 -0500

    login

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -31,12 +32,15 @@ public class SecurityConfiguration extends
     public UserDetailsService userDetailsServiceBean() throws Exception {
         return new SSUserDetailsService(userRepository);
     }
-
+    @Override
+    public void configure(WebSecurity security){
+        security.ignoring().antMatchers("/css/**","/images/**","/js/**", "/resources/**");
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers( "/h2-console/**", "/register").permitAll()
+                .antMatchers( "/login","/h2-console/**","/register", "/*.css", "/*.png", "/*js", "/resources/**").permitAll()
                 .antMatchers("/admin").access("hasAuthority('ADMIN')")
                 .anyRequest().authenticated()
                 .and().formLogin().loginPage("/login").permitAll()
